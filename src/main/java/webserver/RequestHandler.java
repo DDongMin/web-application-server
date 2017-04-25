c068cf149125db52812c85cbd957ea1345c37227
YoonDongmin <ddong4753@gmail.com>
2017-04-25 23:56:35 +0900

    20170425  Commit No 5 Login / Cookie Home

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -15,109 +15,167 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import db.DataBase;
 import model.User;
 import util.HttpRequestUtils;
 import util.IOUtils;
 
-
-
 public class RequestHandler extends Thread {
-    private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
+	private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
-    private Socket connection;
+	private Socket connection;
 
-    public RequestHandler(Socket connectionSocket) {
-        this.connection = connectionSocket;
-    }
+	public RequestHandler(Socket connectionSocket) {
+		this.connection = connectionSocket;
+	}
 
-    public void run() {
-        log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
-                connection.getPort());
+	public void run() {
+		log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
+				connection.getPort());
 
-        try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-        	
-        	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
-        	String line = br.readLine();
-        	if(line==null)
-    			return;
-  
-        	log.debug("http header : {}", line);
-        	//url 추출은 유틸 클래스에서 
-        	//헤더의 첫번재 라인 추출
-        	String url = HttpRequestUtils.getUrl(line);
-        	
-        	//헤더의 두번째 라인부터 (post방식에서 필요함)
-        	Map<String , String> headers = new HashMap<String, String>();
-        	while(!"".equals(line)){
-        		
-        		log.debug("http Header: {}", line);
-        		
-        		line = br.readLine();
-        		String[] headerTokens = line.split(": ");
-        		if(headerTokens.length==2){
-        			headers.put(headerTokens[0], headerTokens[1]);
-        		}
-        		if(line==null)
-        			return;
-        	}
-        	
-        	log.debug("content-length:{}" , headers.get("Content-Length"));
-        	
-        	if(url.startsWith("/user/create")){
-        		
-        		String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
-        		log.debug("Request Body :{}",requestBody);
-        		
-        		Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
-        		User user = new User(params.get("userId"),params.get("password") ,params.get("name") , params.get("email"));
-        		log.debug("User:{}",user);
-        		
-        		url="/index.html";
-        	}
-        	
-        	
-        	//이건 get 방식
-//        	if(url.startsWith("/user/create")){
-//        		int index = url.indexOf("?");
-//        		
-//        		String requestPath = url.substring(0,index);
-//        		String queryString = url.substring(index+1);
-//        		Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
-//        		User user = new User(params.get("userId"),params.get("password") ,params.get("name") , params.get("email"));
-//        		log.debug("User:{}",user);
-//        		
-//        		url="/index.html";
-//        	}
-        	
-            DataOutputStream dos = new DataOutputStream(out);
-//            byte[] body = "Hello World tests!!!!!".getBytes();
-            byte[] body = Files.readAllBytes(new File("./webapp"+url).toPath()); 
-            
-            response200Header(dos, body.length);
-            responseBody(dos, body);
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+		try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+			// TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
 
-    private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
-        try {
-            dos.writeBytes("HTTP/1.1 200 OK \r\n");
-            dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
-            dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
-            dos.writeBytes("\r\n");
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+			BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+			String line = br.readLine();
+			if (line == null)
+				return;
 
-    private void responseBody(DataOutputStream dos, byte[] body) {
-        try {
-            dos.write(body, 0, body.length);
-            dos.flush();
-        } catch (IOException e) {
-            log.error(e.getMessage());
-        }
-    }
+			log.debug("http header : {}", line);
+			// url 추출은 유틸 클래스에서
+			// 헤더의 첫번재 라인 추출
+			String url = HttpRequestUtils.getUrl(line);
+
+			// 헤더의 두번째 라인부터 (post방식에서 필요함)
+			Map<String, String> headers = new HashMap<String, String>();
+			while (!"".equals(line)) {
+
+				log.debug("http Header2: {}", line);
+
+				line = br.readLine();
+				String[] headerTokens = line.split(": ");
+				if (headerTokens.length == 2) {
+					headers.put(headerTokens[0], headerTokens[1]);
+				}
+				if (line == null)
+					return;
+			}
+
+			log.debug("content-length:{}", headers.get("Content-Length"));
+
+			// 이건 post 방
+			if (url.startsWith("/user/create")) {
+
+				String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+				log.debug("Request Body :{}", requestBody);
+
+				Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
+				User user = new User(params.get("userId"), params.get("password"), params.get("name"),
+						params.get("email"));
+				log.debug("User:{}", user);
+				
+				DataBase.addUser(user);
+				
+				DataOutputStream dos = new DataOutputStream(out);
+				response302Header(dos);
+
+			} 
+			else if (url.startsWith("/user/login") ==true && headers.get("Content-Length")!=null) {
+				String requestBody = IOUtils.readData(br, Integer.parseInt(headers.get("Content-Length")));
+				log.debug("Request Body :{}", requestBody);
+
+				Map<String, String> params = HttpRequestUtils.parseQueryString(requestBody);
+				
+				log.debug("Userid:{} , Pasword: {}", params.get("userId"), params.get("password"));
+				
+				User user = DataBase.findUserById(params.get("userId"));
+				
+				if(user==null){
+					log.debug(" User Not Found!!");
+					DataOutputStream dos = new DataOutputStream(out);
+					response302Header(dos);
+				}else if(user.getPassword().equals(params.get("password"))){
+					log.debug(" Log in Success!!");
+					DataOutputStream dos = new DataOutputStream(out);
+					response302HeaderWithCookie(dos, "logined=true");
+				}else{
+					log.debug(" PassWord missmatch!");
+					DataOutputStream dos = new DataOutputStream(out);
+					response302Header(dos);
+				}
+				
+				
+
+			} 
+			else {
+				DataOutputStream dos = new DataOutputStream(out);
+				// byte[] body = "Hello World tests!!!!!".getBytes();
+				byte[] body = Files.readAllBytes(new File("./webapp" + url).toPath());
+
+				response200Header(dos, body.length);
+				responseBody(dos, body);
+			}
+
+			// 이건 get 방식
+			// if(url.startsWith("/user/create")){
+			// int index = url.indexOf("?");
+			//
+			// String requestPath = url.substring(0,index);
+			// String queryString = url.substring(index+1);
+			// Map<String, String> params =
+			// HttpRequestUtils.parseQueryString(queryString);
+			// User user = new User(params.get("userId"),params.get("password")
+			// ,params.get("name") , params.get("email"));
+			// log.debug("User:{}",user);
+			//
+			// url="/index.html";
+			// }
+
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	// 리다이렉트는 302 상태로 해야 한다.
+	private void response302Header(DataOutputStream dos) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Found \r\n");
+			dos.writeBytes("Location: /index.html\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+	//쿠키 유지 
+	private void response302HeaderWithCookie(DataOutputStream dos , String cookie) {
+		try {
+			dos.writeBytes("HTTP/1.1 302 Found \r\n");
+			dos.writeBytes("Location: /index.html\r\n");
+			dos.writeBytes("Set-Cookie: "+cookie+"\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+		
+
+	private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
+		try {
+			dos.writeBytes("HTTP/1.1 200 OK \r\n");
+			dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
+			dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+			dos.writeBytes("\r\n");
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
+
+	private void responseBody(DataOutputStream dos, byte[] body) {
+		try {
+			dos.write(body, 0, body.length);
+			dos.flush();
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		}
+	}
 }
